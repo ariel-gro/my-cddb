b1fc933180cc180cf67d365511aaa93c622be363
llasry@cisco.com <llasry@cisco.com@e2b12618-e9d6-11de-a700-c3e9af1e83fa>
2009-12-31 21:47:24 +0000

    command to refresh the shopping cart

---
+++ trunk/my-cddb/Cd_Store/src/view/UpdateShoppingCart.java
@@ -0,0 +1,25 @@
+package view;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+
+
+public class UpdateShoppingCart extends AbstractHandler
+{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+		IWorkbenchPage page = window.getActivePage();
+		ShoppingCartView view = (ShoppingCartView) page.findView(ShoppingCartView.ID);
+		view.getViewer().refresh();
+		
+		return null;
+	}
+
+}
