86a7f3b0533fa1d66a30804dd8f0b67894ac8605
llasry@cisco.com <llasry@cisco.com@e2b12618-e9d6-11de-a700-c3e9af1e83fa>
2010-01-07 21:58:05 +0000

    Finished search request

--- trunk/my-cddb/Cd_Store/src/model/SearchRequest.java
+++ trunk/my-cddb/Cd_Store/src/model/SearchRequest.java
@@ -1,29 +1,124 @@
 package model;
 
+import java.util.List;
+
 public class SearchRequest implements Comparable<SearchRequest>
 {
-	final public static int HIGH_PRIORITY = 2;
-	final public static int LOW_PRIORITY = 1;
+	public enum Priority {HIGH_PRIORITY, LOW_PRIORITY}
+	public enum SearchType {TOP_20, REGULAR, ADVANCED}	
+	public enum Top20Type {LATEST, MOST_POPULAR};	
+	public enum MusicGenres {ALL, BLUES, CLASSICAL, COUNTRY, DATA, FOLK, JAZZ, NEWAGE, REGGAE, ROCK, SOUNDTRACK, MISC};
+	public enum AdvancedSearchFields {ALBUM_TITLE, ARTIST_NAME, TRACK_TITLE, GENRE, YEAR};
 	
-	private int priority;
+	private int id;
+	private Priority priority;
+	private SearchType searchType;
+	private Top20Type top20Type;
+	private MusicGenres musicGenre;
+	private String regularSearchString;
+	private List<advanceSearchFieldValueBundle> advanceSearchParameters;
 	
-	public SearchRequest(int priority) {
+	// TOP 20 by music musicGenres
+	public SearchRequest(int id, Priority priority, SearchType searchType, MusicGenres musicGenre) 
+	{
+		this.id = id;
 		this.priority = priority;
+		this.searchType = searchType;
+		this.musicGenre = musicGenre;
 	}
-
-
-	@Override
+	
+	// TOP 20 by top20Type
+	public SearchRequest(int id, Priority priority, SearchType searchType, Top20Type top20Type) 
+	{
+		this.id = id;
+		this.priority = priority;
+		this.searchType = searchType;
+		this.top20Type = top20Type;
+	}
+	
+	// Regular search with musicGenres and regularSearchString
+	public SearchRequest(int id, Priority priority, SearchType searchType, MusicGenres musicGenre, String regularSearchString) 
+	{
+		this.id = id;
+		this.priority = priority;
+		this.searchType = searchType;
+		this.musicGenre = musicGenre;
+		this.regularSearchString = regularSearchString;
+	}
+	
+	// Advances search with advanceSearchParameters
+	public SearchRequest(int id, Priority priority, SearchType searchType, List<advanceSearchFieldValueBundle> advanceSearchParameters) 
+	{
+		this.id = id;
+		this.priority = priority;
+		this.searchType = searchType;
+		this.advanceSearchParameters = advanceSearchParameters;
+	}
+	
+	
+	
+	// For priority queue
 	public int compareTo(SearchRequest obj)
 	{
 		SearchRequest that = (SearchRequest) obj; 
 
-        int a = this.priority; 
-        int b = that.priority; 
+		Priority a = this.priority; 
+		Priority b = that.priority; 
  
-        if (a<b) return 1; 
-        if (a>b) return -1; 
+        if (a.ordinal()<b.ordinal()) return 1; 
+        if (a.ordinal()>b.ordinal()) return -1; 
         return 0;
 	}
 	
 	
+	public int getId()
+	{
+		return id;
+	}
+
+	public Priority getPriority()
+	{
+		return priority;
+	}
+
+	public SearchType getSearchType()
+	{
+		return searchType;
+	}
+
+	public Top20Type getTop20Type()
+	{
+		return top20Type;
+	}
+	
+	public MusicGenres getMusicGenres()
+	{
+		return musicGenre;
+	}
+
+	public MusicGenres getMusicGenre()
+	{
+		return musicGenre;
+	}
+
+	public String getRegularSearchString()
+	{
+		return regularSearchString;
+	}
+
+	public List<advanceSearchFieldValueBundle> getAdvanceSearchParameters()
+	{
+		return advanceSearchParameters;
+	}
+
+	
+	//TEMP FOR TESTING
+	public static void main(String [] args)
+	{
+		SearchRequest mySearch = new SearchRequest(0, SearchRequest.Priority.HIGH_PRIORITY, SearchRequest.SearchType.REGULAR, SearchRequest.MusicGenres.ALL);
+	
+		System.out.println(mySearch.getPriority());
+		System.out.println(mySearch.getSearchType());
+		System.out.println(mySearch.getMusicGenres());
+	}
 }
---
+++ trunk/my-cddb/Cd_Store/src/model/advanceSearchFieldValueBundle.java
@@ -0,0 +1,34 @@
+package model;
+
+import model.SearchRequest.AdvancedSearchFields;
+
+public class advanceSearchFieldValueBundle
+{
+	public enum Relation {GREATER, EQUALS, LESSER}
+	
+	private AdvancedSearchFields searchField;
+	private Relation relation;
+	private String value;
+	
+	public advanceSearchFieldValueBundle(AdvancedSearchFields searchField, Relation relation, String value) 
+	{
+		this.searchField = searchField;
+		this.relation = relation;
+		this.value = value;
+	}
+
+	public AdvancedSearchFields getSearchField()
+	{
+		return searchField;
+	}
+
+	public Relation getRelation()
+	{
+		return relation;
+	}
+
+	public String getValue()
+	{
+		return value;
+	}	
+}
