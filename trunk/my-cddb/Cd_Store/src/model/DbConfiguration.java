af60e5e892d1e8d5006cc8c89be3449cc88636d7
llasry@cisco.com <llasry@cisco.com@e2b12618-e9d6-11de-a700-c3e9af1e83fa>
2010-01-09 16:50:43 +0000

    ststic class to hold DB connection details

---
+++ trunk/my-cddb/Cd_Store/src/model/DbConfiguration.java
@@ -0,0 +1,46 @@
+package model;
+
+public class DbConfiguration
+{
+	private static String ipAddress = "";
+	private static int port = 0;
+	private static String user = "";
+	private static String password = "";
+	
+	public static synchronized String getUser()
+	{
+		return user;
+	}
+	public static synchronized void setUser(String user)
+	{
+		DbConfiguration.user = user;
+	}
+	public static synchronized String getPassword()
+	{
+		return password;
+	}
+	public static synchronized void setPassword(String password)
+	{
+		DbConfiguration.password = password;
+	}
+	public static synchronized String getIpAddress()
+	{
+		return ipAddress;
+	}
+	public static synchronized void setIpAddress(String ipAddress)
+	{
+		DbConfiguration.ipAddress = ipAddress;
+	}
+	
+	public static synchronized int getPort()
+	{
+		return port;
+	}
+	
+	public static synchronized void setPort(int port)
+	{
+		DbConfiguration.port = port;
+	}
+	
+	
+}
