7ffe8684294a2942e19a8fdb337ad3260c14fb53
llasry@cisco.com <llasry@cisco.com@e2b12618-e9d6-11de-a700-c3e9af1e83fa>
2010-01-12 21:09:33 +0000

    command for db import

---
+++ trunk/my-cddb/Cd_Store/src/view/commands/OpenDbImportWizard.java
@@ -0,0 +1,23 @@
+package view.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import view.views.DbImportWizardMain;
+
+public class OpenDbImportWizard extends AbstractHandler
+{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
+		DbImportWizardMain wizard = new DbImportWizardMain();
+		WizardDialog dialog = new WizardDialog(HandlerUtil.getActiveShell(event), wizard);
+		dialog.open();
+		return null;
+	}
+
+}
