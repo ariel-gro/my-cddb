751be18080c531a9ac004d07b51d0a2dddc6802c
arielthepriest <arielthepriest@e2b12618-e9d6-11de-a700-c3e9af1e83fa>
2010-01-14 18:48:52 +0000

    ...

--- trunk/my-cddb/Cd_Store/src/model/SqlStatement.java
+++ trunk/my-cddb/Cd_Store/src/model/SqlStatement.java
@@ -4,24 +4,26 @@ import java.util.Hashtable;
 
 public class SqlStatement
 {
-	public enum SearchType {INSERT_SINGLE, INSERT_BULK, QUERY}	
+	public enum queryType {INSERT_SINGLE, INSERT_BULK, QUERY}	
 	
-	private SearchType sType;
+	private queryType queryType;
 	private String stmt;
 	private Hashtable<String,String[]> tuples;
+	private int requestId;
 	
-	public SqlStatement(SearchType sType, String stmt, Hashtable<String, String[]> tuples) {
-		this.sType = sType;
+	public SqlStatement(queryType queryType, String stmt, Hashtable<String, String[]> tuples, int requestId) {
+		this.queryType = queryType;
 		this.stmt = stmt;
 		this.tuples = tuples;
+		this.requestId = requestId;
 	}
 
-	public SearchType getsType() {
-		return sType;
+	public queryType getQueryType() {
+		return queryType;
 	}
 
-	public void setsType(SearchType sType) {
-		this.sType = sType;
+	public void setQueryType(queryType queryType) {
+		this.queryType = queryType;
 	}
 
 	public String getStmt() {
@@ -38,6 +40,14 @@ public class SqlStatement
 
 	public void setTuples(Hashtable<String, String[]> tuples) {
 		this.tuples = tuples;
+	}
+
+	public int getRequestId() {
+		return requestId;
+	}
+
+	public void setRequestId(int requestId) {
+		this.requestId = requestId;
 	}
 	
 	
