d21820f36c1c66cf01696434d5d806a13c38398c
arielthepriest <arielthepriest@e2b12618-e9d6-11de-a700-c3e9af1e83fa>
2010-01-15 10:02:13 +0000

    ..

--- trunk/my-cddb/Cd_Store/src/controller/connectionManager.java
+++ trunk/my-cddb/Cd_Store/src/controller/connectionManager.java
@@ -1,22 +1,50 @@
 package controller;
 
+import java.sql.ResultSet;
 import java.util.PriorityQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 
 import model.SqlStatement;
 
-public class connectionManager {
+public class connectionManager implements Runnable{
 	
-	private DbConnector[] connections;
-	private Thread[] connThreads;
-	private static PriorityQueue<SqlStatement> theQueue = null;
-	private static int requestId = 0;
+	private ExecutorService connThreads;
+	private int threadNum;
+	private static LinkedBlockingQueue<SqlStatement> theQueue = null;
 	
 	public static synchronized void insertToQueue (SqlStatement sqlStatment){
 		if (theQueue == null)
-			theQueue = new PriorityQueue<SqlStatement>();
-		
-		theQueue.add(sqlStatment);
+			theQueue = new LinkedBlockingQueue<SqlStatement>();
+		try {
+			theQueue.put(sqlStatment);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
+
+	public void run() {
+		//initialize class fields
+		if (this.connThreads == null){
+			this.connThreads = Executors.newCachedThreadPool();
+			this.threadNum = 0;
+		}
+				
+		//main loop
+		SqlStatement stmt;
+		while (true){
+			try {
+				stmt = theQueue.take();				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 
 	//TODO: handle threads, handle queue extraction
 }
