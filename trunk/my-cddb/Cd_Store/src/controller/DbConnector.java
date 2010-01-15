fba714c3e93a408adf3f2edc60990722af99c800
arielthepriest <arielthepriest@e2b12618-e9d6-11de-a700-c3e9af1e83fa>
2010-01-15 16:12:52 +0000

    one step closer to concurrency with connections

--- trunk/my-cddb/Cd_Store/src/controller/DbConnector.java
+++ trunk/my-cddb/Cd_Store/src/controller/DbConnector.java
@@ -2,14 +2,13 @@ package controller;
 import java.sql.*;
 import java.util.concurrent.Callable;
 
-import javax.swing.text.StyleContext.SmallAttributeSet;
 
 import model.DbConfiguration;
 import model.SqlStatement;
 import model.SqlStatement.queryType;
 
 
-public class DbConnector implements Callable<ResultSet>{
+public class DbConnector implements Runnable{
 
 	private Connection connection;	// DB connection
 
@@ -22,75 +21,14 @@ public class DbConnector implements Callable<ResultSet>{
 		this.stmt = null;
 	}
 
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
 	public void setStatement(SqlStatement stmt){
 		this.stmt = stmt;
 	}
-	/**
-	 * 
-	 * @return the connection (null on error)
-	 */
-	public void openConnection()
-	{
-
-		// loading the driver
-		try
-		{
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-		}
-		catch (ClassNotFoundException e)
-		{
-			System.out.println("Unable to load the Oracle JDBC driver..");
-			java.lang.System.exit(0); 
-		}
-		System.out.println("Driver loaded successfully");
-
-
-		// creating the connection
-		System.out.print("Trying to connect.. ");
-		try
-		{
-			String jdbcurl =
-				"jdbc:oracle:thin:" + DbConfiguration.getIpAddress()+":" + DbConfiguration.getPort() +
-				"/" + DbConfiguration.getDb();
-			
-			connection =
-				DriverManager.getConnection(jdbcurl, DbConfiguration.getUser(), DbConfiguration.getPassword());
-		}
-		catch (SQLException e)
-		{
-			System.out.println("Unable to connect - " + e.toString());
-			java.lang.System.exit(0); 
-		}
-		catch (Exception e)
-		{
-			System.err.println("ERROR: " + e);
-		}
-		System.out.println("Connected!");
-
-	}
-
-	/**
-	 * Closes the connection to the server.
-	 */
-	public void close() {
-		try {
-			connection.close();
-		} catch (SQLException e) {
-			System.err.println("ERROR: Couldn't close connection to SQL-server. " + e);
-		}
-		System.out.println("connection closed successfully");
-	}
 	
-	//TODO: create user method
-	
-
-	public void closeStatement() {
-		try {
-			this.ps.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
 
 	/**
 	 * Execute the prepared statement.
@@ -105,46 +43,21 @@ public class DbConnector implements Callable<ResultSet>{
 		return null;
 	}
 
-	/**
-	 * Execute the prepared statement. Must be an SQL INSERT, UPDATE or DELETE
-	 * statement; or an SQL statement that returns nothing, such as a DDL statement.
-	 */
-	public int executeUpdate() {
-		try {
-			return ps.executeUpdate();
-		} catch (SQLException e) {
-			System.err.println("ERROR: (executeUpdate). " + e);
-		}
-
-		return -1;
-	}
-
-	/**
-	 * Wrapper for the PreparedStatement.getGeneratedKeys() method.
-	 */
-	public ResultSet getGeneratedKeys() {
-		try {
-			return ps.getGeneratedKeys();
-		} catch (SQLException e) {
-			System.err.println("ERROR: (getGeneratedKeys). " + e);
-		}
-
-		return null;
-	}
 	
-	public ResultSet call(){
+	public void run(){
 		
 		queryType qt = stmt.getQueryType();
 		
 		switch (qt){
 		case INSERT_BULK:
-			return executeBulkInsert(stmt);
+			executeBulkInsert(stmt);
+			break;
 		case INSERT_SINGLE:
-			return executeSingleInsert(stmt);
+			executeSingleInsert(stmt);
+			break;
 		case QUERY:
-			return executeQuery(stmt);
-		default:
-			return null;
+			executeQuery(stmt);
+			break;
 		}
 	}
 
--- trunk/my-cddb/Cd_Store/src/controller/connectionManager.java
+++ trunk/my-cddb/Cd_Store/src/controller/connectionManager.java
@@ -1,50 +1,148 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.util.PriorityQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.PriorityBlockingQueue;
+import java.sql.SQLException;
+import java.util.Vector;
+import java.util.concurrent.*;
 
+
+import model.DbConfiguration;
 import model.SqlStatement;
 
-public class connectionManager implements Runnable{
+public class connectionManager {
+
+	private Executor connThreads;
+	private LinkedBlockingQueue<Connection> connQueue;
+	private Vector<Connection> conVector;
+	private static LinkedBlockingQueue<SqlStatement> queryQueue = null;
+	private static LinkedBlockingQueue<ResultSet> resultQueue = null;
+	private int numOfConnections = 0;
 	
-	private ExecutorService connThreads;
-	private int threadNum;
-	private static LinkedBlockingQueue<SqlStatement> theQueue = null;
-	
+	private boolean timeToQuit = false;
+
 	public static synchronized void insertToQueue (SqlStatement sqlStatment){
-		if (theQueue == null)
-			theQueue = new LinkedBlockingQueue<SqlStatement>();
+		if (queryQueue == null)
+			queryQueue = new LinkedBlockingQueue<SqlStatement>();
+		
 		try {
-			theQueue.put(sqlStatment);
+			queryQueue.put(sqlStatment);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static synchronized void insertToResultQueue (ResultSet result){
+		if (resultQueue == null)
+			resultQueue = new LinkedBlockingQueue<ResultSet>();
+		
+		try {
+			resultQueue.put(result);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void run() {
+	public synchronized void start() {
 		//initialize class fields
-		if (this.connThreads == null){
-			this.connThreads = Executors.newCachedThreadPool();
-			this.threadNum = 0;
+		if (this.connThreads == null)
+			this.connThreads = Executors.newFixedThreadPool(10);
+
+		if (queryQueue == null)
+			queryQueue = new LinkedBlockingQueue<SqlStatement>();
+
+		if (connQueue == null){
+			connQueue = new LinkedBlockingQueue<Connection>();
+			conVector = new Vector<Connection>(10);
+			this.openConnection();
 		}
-				
-		//main loop
-		SqlStatement stmt;
-		while (true){
+		
+		Thread queryListner = new Thread(new HandleQueryQueue());
+		queryListner.start();
+		
+		while (!timeToQuit){
+			//threads are working
+		}
+		
+		//end work and threads
+		for (int i=0; i < numOfConnections; i++) {
 			try {
-				stmt = theQueue.take();				
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+				conVector.elementAt(i).close();
+			}
+			catch (SQLException e) {
+				System.err.println("ERROR: Couldn't close connection to SQL-server. " + e);
 			}
 		}
 	}
 	
+	public synchronized void quit(){
+		this.timeToQuit = true;
+	}
+	
+	private synchronized void openConnection()
+	{
+
+		// loading the driver
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}
+		catch (ClassNotFoundException e)
+		{
+			System.out.println("Unable to load the Oracle JDBC driver..");
+			java.lang.System.exit(0); 
+		}
+		System.out.println("Driver loaded successfully");
+
+
+		// creating the connection
+		System.out.print("Trying to connect.. ");
+		try
+		{
+			String jdbcurl =
+				"jdbc:oracle:thin:" + DbConfiguration.getIpAddress()+":" + DbConfiguration.getPort() +
+				"/" + DbConfiguration.getDb();
+			
+			Connection connection =
+				DriverManager.getConnection(jdbcurl, DbConfiguration.getUser(), DbConfiguration.getPassword());
+			
+			this.connQueue.put(connection);
+			this.conVector.add(numOfConnections, connection);
+			numOfConnections++;
+		}
+		catch (SQLException e)
+		{
+			System.out.println("Unable to connect - " + e.toString());
+			java.lang.System.exit(0); 
+		}
+		catch (Exception e)
+		{
+			System.err.println("ERROR: " + e);
+		}
+		System.out.println("Connected!");
+	}
+
+	private class HandleQueryQueue implements Runnable {
+
+		public synchronized void run() {
+			SqlStatement stmt;
+			DbConnector con = new DbConnector();
+			while (!timeToQuit){
+				try {
+					stmt = queryQueue.take();
+					if ((numOfConnections < 10) && connQueue.isEmpty())
+						openConnection();
+					con.setConnection(connQueue.take());
+					con.setStatement(stmt);
+					connThreads.execute(con);
+				}
+				catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 	//TODO: handle threads, handle queue extraction
 }
